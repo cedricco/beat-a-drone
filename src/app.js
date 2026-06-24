1fe79e9b3c45bc860298032cbd7c2e13b9e77f63
cedricco <cedric.codet@gmail.com>
2026-06-24 20:20:48 +0200

    Use standardized audio context

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import { Sequencer, Soundfont, CacheStorage } from "smplr";
+import { AudioContext } from "standardized-audio-context";
+
 
 const context = new AudioContext();
 const storage = CacheStorage();
@@ -6,10 +8,11 @@ const storage = CacheStorage();
 const instru1 = Soundfont(context, { instrument: "bagpipe", storage });
 const instru2 = Soundfont(context, { instrument: "taiko_drum", storage });
 
-instru1.output.volume = 80 // Max 127
-
 await instru1.ready
 await instru2.ready
+
+instru1.output.volume = 80 // Max 127
+
 
 const seq = Sequencer(context, { bpm: 100, loop: true });
 seq.addTrack(instru1, [
@@ -27,22 +30,22 @@ seq.addTrack(instru1, [
   { note: "E4", at: "4:1", duration: "1m" },
 ]);
 seq.addTrack(instru2, [
-  { note: "C4", at: "1:1", duration: "1m" },
-  { note: "C5", at: "1:2", duration: "1m" },
-  { note: "C5", at: "1:3", duration: "1m" },
-  { note: "C5", at: "1:4", duration: "1m" },
-  { note: "C4", at: "2:1", duration: "1m" },
-  { note: "C5", at: "2:2", duration: "1m" },
-  { note: "C5", at: "2:3", duration: "1m" },
-  { note: "C5", at: "2:4", duration: "1m" },
-  { note: "C4", at: "3:1", duration: "1m" },
-  { note: "C5", at: "3:2", duration: "1m" },
-  { note: "C5", at: "3:3", duration: "1m" },
-  { note: "C5", at: "3:4", duration: "1m" },
-  { note: "C4", at: "4:1", duration: "1m" },
-  { note: "C5", at: "4:2", duration: "1m" },
-  { note: "C5", at: "4:3", duration: "1m" },
-  { note: "C5", at: "4:4", duration: "1m" },
+  { note: "C4", at: "1:1", duration: "4n" },
+  { note: "C5", at: "1:2", duration: "4n" },
+  { note: "C5", at: "1:3", duration: "4n" },
+  { note: "C5", at: "1:4", duration: "4n" },
+  { note: "C4", at: "2:1", duration: "4n" },
+  { note: "C5", at: "2:2", duration: "4n" },
+  { note: "C5", at: "2:3", duration: "4n" },
+  { note: "C5", at: "2:4", duration: "4n" },
+  { note: "C4", at: "3:1", duration: "4n" },
+  { note: "C5", at: "3:2", duration: "4n" },
+  { note: "C5", at: "3:3", duration: "4n" },
+  { note: "C5", at: "3:4", duration: "4n" },
+  { note: "C4", at: "4:1", duration: "4n" },
+  { note: "C5", at: "4:2", duration: "4n" },
+  { note: "C5", at: "4:3", duration: "4n" },
+  { note: "C5", at: "4:4", duration: "4n" },
 ]);
 
 const startBtn = document.querySelector("#startBtn")
