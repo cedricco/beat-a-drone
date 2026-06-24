e54b6955571a7eb7218728f9fa37300249c1bc4c
cedricco <cedric.codet@gmail.com>
2026-06-24 19:53:54 +0200

    bagpipe test ! and taiko drum

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
-import { Soundfont } from "smplr";
+import { Sequencer, Soundfont, CacheStorage } from "smplr";
 
 const context = new AudioContext();
-const marimba = Soundfont(context, { instrument: "marimba" });
+const storage = CacheStorage();
+
+const instru1 = Soundfont(context, { instrument: "bagpipe", storage });
+const instru2 = Soundfont(context, { instrument: "taiko_drum", storage });
+
+instru1.output.volume = 80 // Max 127
+
+await instru1.ready
+await instru2.ready
+
+const seq = Sequencer(context, { bpm: 100, loop: true });
+seq.addTrack(instru1, [
+  { note: "C4", at: "1:1", duration: "1m" },
+  { note: "E4", at: "1:1", duration: "1m" },
+  { note: "G4", at: "1:1", duration: "1m" },
+  { note: "B5", at: "2:1", duration: "1m" },
+  { note: "E4", at: "2:1", duration: "1m" },
+  { note: "G4", at: "2:1", duration: "1m" },
+  { note: "B5", at: "3:1", duration: "1m" },
+  { note: "D5", at: "3:1", duration: "1m" },
+  { note: "G4", at: "3:1", duration: "1m" },
+  { note: "A4", at: "4:1", duration: "1m" },
+  { note: "C4", at: "4:1", duration: "1m" },
+  { note: "E4", at: "4:1", duration: "1m" },
+]);
+seq.addTrack(instru2, [
+  { note: "C4", at: "1:1", duration: "1m" },
+  { note: "C5", at: "1:2", duration: "1m" },
+  { note: "C5", at: "1:3", duration: "1m" },
+  { note: "C5", at: "1:4", duration: "1m" },
+  { note: "C4", at: "2:1", duration: "1m" },
+  { note: "C5", at: "2:2", duration: "1m" },
+  { note: "C5", at: "2:3", duration: "1m" },
+  { note: "C5", at: "2:4", duration: "1m" },
+  { note: "C4", at: "3:1", duration: "1m" },
+  { note: "C5", at: "3:2", duration: "1m" },
+  { note: "C5", at: "3:3", duration: "1m" },
+  { note: "C5", at: "3:4", duration: "1m" },
+  { note: "C4", at: "4:1", duration: "1m" },
+  { note: "C5", at: "4:2", duration: "1m" },
+  { note: "C5", at: "4:3", duration: "1m" },
+  { note: "C5", at: "4:4", duration: "1m" },
+]);
 
 const startBtn = document.querySelector("#startBtn")
 startBtn.addEventListener("click", (e) => {
-    marimba.start({ note: 60, velocity: 80 });
+    seq.start();
 })
